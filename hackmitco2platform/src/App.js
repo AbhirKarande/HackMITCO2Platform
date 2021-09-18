659f46c66637363cf181768007c663990c8b9d3d
aryanmagoon-ds <aryanchesscom@gmail.com>
2021-09-18 13:55:03 -0400

    added google authentication

--- hackmitco2platform/src/App.js
+++ hackmitco2platform/src/App.js
@@ -1,36 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from "./components/authentication/Signup";
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "./components/authentication/AuthContext"
-import Login from "./components/authentication/Login"
-import PrivateRoute from "./components/authentication/PrivateRoute"
-import ForgotPassword from "./components/authentication/ForgotPassword"
-import UpdateProfile from "./components/authentication/UpdateProfile"
-import Dashboard from "./components/home/Dashboard"
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+import Dashboard from "./Dashboard";
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
-  )
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/reset" component={Reset} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
-
 export default App;
